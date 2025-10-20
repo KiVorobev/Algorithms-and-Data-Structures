9f7784103303b0619a386075ffdd959e87db617b
Kirill <kirill.vorobyev2002@yandex.ru>
2025-10-20 21:33:35 +0300

    leetcode: add task 66

---
+++ leetcode/task66/Solution.java
@@ -0,0 +1,19 @@
+package task66;
+
+class Solution {
+    public int[] plusOne(int[] digits) {
+        for (int i = digits.length - 1; i >= 0; i--) {
+            if (digits[i] < 9) {
+                digits[i]++;
+                return digits;
+            }
+            digits[i] = 0;
+        }
+        digits = new int[digits.length + 1];
+        for (int i = 0; i < digits.length; i++) {
+            if (i == 0) digits[i] = 1;
+            else digits[i] = 0;
+        }
+        return digits;
+    }
+}
