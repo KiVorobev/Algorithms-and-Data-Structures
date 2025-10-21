4f822e3cf3d2d61d377a769c8bc5d4ee8cb3e8b3
Kirill <kirill.vorobyev2002@yandex.ru>
2025-10-21 19:00:42 +0300

    leetcode: add task 104

---
+++ leetcode/task104/Solution.java
@@ -0,0 +1,10 @@
+package task104;
+
+class Solution {
+    public int maxDepth(TreeNode root) {
+        if (root == null) return 0;
+        int leftDepth = maxDepth(root.left);
+        int rightDepth = maxDepth(root.right);
+        return Math.max(leftDepth, rightDepth) + 1;
+    }
+}
